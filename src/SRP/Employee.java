5ade67ea9477b85a90cfd899905248be5340748f
Omkar3843 <deshmukhomkar772@gmail.com>
2024-06-02 13:30:56 +0530

    2nd

--- src/SRP/Employee.java
+++ src/SRP/Employee.java
@@ -1,2 +1,24 @@
-package SRP;public class Employee {
+package SRP;
+
+public class Employee {
+
+    private int id;
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String fetchBioData() {
+        return "Some bio data";
+    }
+
+    public double calculateSalary() {
+        return 0;
+    }
+
+    public void printPerformanceData() {
+        System.out.println("Some performance related data");
+    }
+
+
 }
